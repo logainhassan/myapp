cff8f35bcb91efbd3e0f2a97607bfb100b69319a
logainhassan <logeenhassanmostafa96@gmail.com>
2020-04-25 11:49:23 -0700

    sign up and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :users, only: %i[new create]
+  resource :session, only: %i[new create destroy]
+
   get 'welcome/index'
- 
-  
 	resources :articles do
 	  resources :comments
 	end
