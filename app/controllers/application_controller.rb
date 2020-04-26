963cf502a87b395a3c7eb57f985620463126c8a5
logainhassan <logeenhassanmostafa96@gmail.com>
2020-04-26 07:58:15 -0700

    remove user login and registeration and begin with gem-devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  # protect_from_forgery with: :exception
+  # helper_method :current_user, :logged_in?
 
-  private
+  # private
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
+  # def current_user
+  #   @current_user ||= User.find_by(id: session[:user_id])
+  # end
 
-  def logged_in?
-    current_user
-  end
+  # def logged_in?
+  #   current_user
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :models
   get 'sessions/new'
   resources :users, only: %i[new create]
   resource :session, only: %i[new create destroy]
