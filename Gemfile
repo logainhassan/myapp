80593956def69e36ae3f5da963df7bc20d6daed1
logainhassan <logeenhassanmostafa96@gmail.com>
2020-04-27 14:43:34 -0700

    active_model_serializers

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'puma', '~> 4.1'
 gem 'bcrypt', '~> 3.1.7'
 gem 'jwt'
 gem 'simple_command'
+gem 'active_model_serializers'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- post 'authenticate', to: 'authentication#authenticate'
+post 'authenticate', to: 'authentication#authenticate'
 end
+ 
+# eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1ODgxMDkwNzZ9.PgBAmCWHFk8S7fN5JUNO0FOOoFMe-mRvvsphiZ-lJpQ
